702b801c70c1eb0d38f3b98296cf8e3a1c4bd9f5
iPuuuuu <161623620+iPuuuuu@users.noreply.github.com>
2024-08-10 07:40:49 +0000

    test

---
+++ test.py
@@ -0,0 +1,26 @@
+import functools
+from typing import Optional, Callable
+
+import numpy as np
+import jax
+from jax import lax, random, numpy as jnp
+
+import flax
+from flax import struct, traverse_util, linen as nn
+from flax.core import freeze, unfreeze
+from flax.training import train_state, checkpoints
+
+import optax # Optax for common losses and optimizers.
+
+from jax.sharding import Mesh, PartitionSpec, NamedSharding
+from jax.lax import with_sharding_constraint
+from jax.experimental import mesh_utils
+
+device_mesh = mesh_utils.create_device_mesh((2, 4))
+print(device_mesh)
+
+mesh = Mesh(devices=device_mesh, axis_names=('data', 'model'))
+print(mesh)
+
+def mesh_sharding(pspec: PartitionSpec) -> NamedSharding:
+  return NamedSharding(mesh, pspec)
